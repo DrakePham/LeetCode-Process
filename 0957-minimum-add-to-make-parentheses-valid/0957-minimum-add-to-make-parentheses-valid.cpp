31372e03e13634afff4cc3b90469f09229d58478
Drake Pham <89468358+DrakePham@users.noreply.github.com>
2024-11-03 13:56:04 -0600

    Time: 0 ms (100%), Space: 8 MB (10.16%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         stack <char> st;
-        int result = 0;
         for(int i = 0; i < s.length(); i++){
             if(!st.empty() && st.top() == '(' && s[i] == ')'){
-                result--;
                 st.pop();
                 continue;
             }
             st.push(s[i]);
-            result++;
         }
-        return result;
+        return st.size();
     }
 };
