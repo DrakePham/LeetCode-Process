d39265e36e8d3fe2384fdd0a509d1d2b769512e5
Drake Pham <89468358+DrakePham@users.noreply.github.com>
2024-11-04 07:59:23 -0600

    Time: 1 ms (82.18%), Space: 13.2 MB (67.32%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp = {nums[0]};
+        int result = 1;
+        for(int i = 1; i < nums.size(); i++){
+            auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
+            if(it == dp.end()) {
+                dp.push_back(nums[i]);
+                result = max(result, (int)dp.size());
+            }else{
+                int index = it - dp.begin();
+                dp[index] = nums[i];
+                result = max(result, index + 1);
+            }
+        }
+
+        return result;
+    }
+};
+
+// plan:
+
+// if we want to find the max sub array of the current index, we need to find the max subarray of the previous index
+
+// ex [2,1,3,4]
+
+// dp[3]=1
+// dp[2]= max(1+dp[3])
+//               3<4
+// dp[1] = max(1+dp[2],1+dp[3])
+//                1<3   1<4
+// dp[0]= max(1   ,1+dp[2],1+dp[3])   
+//            2>1    2<3    2<4
+
+// [2,1,3,4] return 3
+
+// [1,1,1,1] dp
+
+// i=2 j=3
+// dp[2]= max(1,1+1) =2
+// i=1 j=2 j=3
+// dp[1]= max(1,1+2,1+1)=3
+// i=0 j=2 j=3
+// dp[0]= max(1,1+2,1+1) =3
+// return 3
+
