e2365d24c15187f253d928413a003aa1160659bf
Drake Pham <89468358+DrakePham@users.noreply.github.com>
2026-02-24 13:03:16 +0700

    Time: 0 ms (100%), Space: 13.2 MB (98.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,12 +3,23 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int t = 0;
         int b = matrix.size() - 1;
-        for(int i = 0; i <= b; i++){
-            if(search(matrix[i], target)) return true;
-        }
-        return false;
-    }
+        // for(int i = 0; i <= b; i++){
+        //     if(search(matrix[i], target)) return true;
+        // }
+        // return false;
 
+        while( t <= b){
+            int mid = t + (b - t)/2;
+            int number = matrix[mid][0];
+            if(number > target) b = mid - 1;
+            else t = mid + 1;
+        }
+        return search(matrix[b >= 0 ? b : 0], target);
+    }
+// 0 0 1
+// 2 0
+// 1 0
+ 
     bool search(vector<int>& line, int target){
         int r = line.size() - 1;
         int l = 0;
