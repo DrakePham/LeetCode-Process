e8de529967a253086996474e02f748df3545edf7
Drake Pham <89468358+DrakePham@users.noreply.github.com>
2026-02-25 17:00:50 +0700

    Time: 7 ms (70.13%), Space: 23 MB (45.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -14,28 +14,11 @@ public:
                 l = k + 1;
             }
         }
-        return result;
+        return l;
     }
-    int findMax (vector<int>& piles){
-        int maxNum = INT_MIN;
-        for(auto& num:piles){
-            maxNum = max(num, maxNum);
-        }
-        return maxNum;
-    }
-    
-    int findMin (vector<int>& piles){
-        int minNum = INT_MAX;
-        for(auto& num:piles){
-            minNum = min(num, minNum);
-        }
-        return minNum;
-    }
-
     bool canEat(vector<int>& piles, int h, int k){
         for(auto& pile:piles){
             h -= (pile - 1)/k + 1;
-            // cout<<h<<"-";
         }
         return h >= 0;
     }
